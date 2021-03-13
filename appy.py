11098e04cd2286d75b9e1c57aa0d7d67bdcd8b06
sir-omoreno <ozmoreno@gmail.com>
2021-03-13 00:10:03 -0500

    api dependancies

--- appy.py
+++ appy.py
@@ -4,3 +4,13 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 import datetime as dt
+
+#################################################
+# Database Setup
+#################################################
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+measurement = Base.classes.measurement
+station = Base.classes.station
